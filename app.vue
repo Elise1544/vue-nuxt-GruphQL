39171f02269a21cc3300e9c017ddd855a9523313
Elizaveta Zabrodskaya <e_zabrodskaya@lesta.group>
2023-07-04 10:48:22 +0300

    build(frontned): add lint

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <div class="wrapper-text">123</div>
 </template>
+
+<script setup>
+const a = 123
+console.log(a)
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  &-text {
+    width: 10px;
+    height: 10px;
+    color: red;
+  }
+}
+</style>
